70433bc31082f5956a80f56553938e32a075b3b7
Jason Meredith <jason-meredith@live.com>
2023-08-13 19:30:22 -0400

    force dynamic mode for api endpoint

--- app/api/route.jsx
+++ app/api/route.jsx
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
 import { fetchAllCompetitions } from '@/util/wom-helper';
+
+export const dynamic = 'force-dynamic'
  
 export async function GET() {
   const scores = await fetchAllCompetitions(25);
