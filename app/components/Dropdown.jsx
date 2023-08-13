22032232eab2fffb789379d16976cb4822c6f7e3
Jason Meredith <jasonmeredith93+social-btw@gmail.com>
2023-08-12 21:55:48 -0400

    remove xp from title

--- app/components/Dropdown.jsx
+++ app/components/Dropdown.jsx
@@ -10,7 +10,7 @@ export default function Dropdown({ title, skills, competition }) {
   if(skills.length > 1) {
     return (
       <div class="dropdown">
-        <button class="dropbtn">{title} XP ▼</button>
+        <button class="dropbtn">{title} ▼</button>
         <div class="dropdown-content">
           <Link href="/">{competition}</Link>
           {options}
