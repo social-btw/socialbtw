d53fad9c878b07e4e683ce58dc24f80a5e6b39d4
Jason Meredith <jason-meredith@live.com>
2023-08-28 13:18:42 -0400

    remove put condition

--- pages/api/discord.jsx
+++ pages/api/discord.jsx
@@ -133,7 +133,7 @@ export default async function handler(req, res) {
       });
     }
   // Async deploy request 
-  } else if(req.method == 'PUT') {
+  } else {
     const { token } = req.body
 
     if(token == undefined) {
