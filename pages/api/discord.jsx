fe49093b5ac1f431ff6f093fba4167b9b585138f
Jason Meredith <jason-meredith@live.com>
2023-08-28 13:15:29 -0400

    async deploys

--- pages/api/discord.jsx
+++ pages/api/discord.jsx
@@ -47,6 +47,39 @@ const addSkill = (name, womId, pointsMultiplier) => {
   return response;
 }
 
+const updateMessage = async (updatedMessage, token) => {
+  const appId = process.env.DISCORD_APP_ID
+  const endpoint = `webhooks/${appId}/${token}/messages/@original`
+  const body = JSON.stringify({
+    content: updatedMessage
+  })
+
+  const headers = {
+    Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+    'Content-Type': 'application/json; charset=UTF-8'
+  }
+
+  return fetch(`https://discord.com/api/v10/${endpoint}`, {
+    body,
+    headers,
+    method: 'PATCH'
+  })
+}
+
+const performAsyncDeploy = async (token) => {
+  const body = JSON.stringify({
+    token
+  })
+
+  fetch(`${process.env.HOSTNAME}/api/discord`, {
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    method: 'PUT',
+    body
+  })
+}
+
 const deployValues = async () => {
   const result = await deploy() ? 'Success :tada:' : 'Failure :crying_cat_face:';
 
@@ -54,46 +87,71 @@ const deployValues = async () => {
 }
 
 export default async function handler(req, res) {
-  const { type, data } = req.body;
+  // The initial interaction request from Discord
+  if(req.method == 'POST') {
+    const { type, data, token } = req.body;
 
-  if (verify(req) == false || req.method !== 'POST') {
-    return res.status(401).json({ error: 'Bad signature' })
-  }
+    // Verify the request
+    if (verify(req) == false) {
+      return res.status(401).json({ error: 'Bad signature' })
+    }
+  
+    if (type === InteractionType.PING) {
+      return res.status(200).json({ type: InteractionResponseType.PONG });
+    }
+  
+    // Commands
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name, options } = data;
 
-  if (type === InteractionType.PING) {
-    return res.status(200).json({ type: InteractionResponseType.PONG });
-  }
+      let response;
+  
+      switch(name) {
+        case 'setup':
+          response = setupComp(options[0].value)
+          break;
+        case 'add':
+          const skillName = options.find(option => option.name === 'name').value
+          const womId = options.find(option => option.name === 'wom_id').value
+          const pointsMultiplier = options.find(option => option.name === 'points_multiplier')?.value
+  
+          response = addSkill(skillName, womId, pointsMultiplier)
+          break;
+        case 'deploy':
+          await performAsyncDeploy(token);
 
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name, options } = data;
+          return res.status(200).json({
+            type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
+          });
+      }
+  
+      return res.status(200).json({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: response
+        }
+      });
+    }
+  // Async deploy request 
+  } else if(req.method == 'PUT') {
+    const { token } = req.body
 
-    console.log(req.body)
-
-    let response;
-
-    switch(name) {
-      case 'setup':
-        response = setupComp(options[0].value)
-        break;
-      case 'add':
-        const skillName = options.find(option => option.name === 'name').value
-        const womId = options.find(option => option.name === 'wom_id').value
-        const pointsMultiplier = options.find(option => option.name === 'points_multiplier')?.value
-
-        response = addSkill(skillName, womId, pointsMultiplier)
-        break;
-      case 'deploy':
-        response = await deployValues()
-        break;
+    if(token == undefined) {
+      res.status(400).json({
+        success: false
+      });
     }
 
+    const message = await deployValues();
+
+    await updateMessage(message, token)
+    .then(res => {
+      console.log(res.status)
+      return res.text()
+    }).then(body => console.log(body))
+
     return res.status(200).json({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: response,
-      },
+      success: true
     });
   }
-
-  return res.status(200).json({ type: InteractionResponseType.PONG });
 }
